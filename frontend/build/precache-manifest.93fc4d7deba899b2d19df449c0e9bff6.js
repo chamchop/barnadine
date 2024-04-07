ff38fcf8676eb51a4103b2f9932e94a8052492d6
chamchop <ocfmiller@protonmail.com>
2024-04-07 13:44:58 +0100

    frontend: build files

---
+++ frontend/build/precache-manifest.93fc4d7deba899b2d19df449c0e9bff6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de24cf77177dd0ac8edc91f8a22518d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4442824c4449bdca5022",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "3b05a1d8a48098db244c",
+    "url": "/static/css/main.fd927632.chunk.css"
+  },
+  {
+    "revision": "4442824c4449bdca5022",
+    "url": "/static/js/2.bdf6d552.chunk.js"
+  },
+  {
+    "revision": "42fbc45beaf636618f41d10042136c38",
+    "url": "/static/js/2.bdf6d552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b05a1d8a48098db244c",
+    "url": "/static/js/main.9b219104.chunk.js"
+  },
+  {
+    "revision": "1ff98e69d4eceeaf1a39",
+    "url": "/static/js/runtime-main.39f224c8.js"
+  }
+]);
