c4c59262225cfcd0aacdce6309fbd857e7134365
chamchop <ocfmiller@protonmail.com>
2024-04-13 13:00:47 +0100

    db: postgres rds

--- ci/terraform/main.tf
+++ ci/terraform/main.tf
@@ -1,3 +1,96 @@
-provider "aws" {
-  region = "eu-west-2"
+ resource "aws_db_instance" "barnardine-dev" {
+  allocated_storage      = 10
+  identifier             = "barnardine-dev"
+  db_subnet_group_name   = aws_db_subnet_group.default.id
+  engine                 = "postgres"
+  engine_version         = "14"
+  instance_class         = "db.t3.micro"
+  username               = "postgres"
+  password               = "postgres"
+  vpc_security_group_ids = [aws_security_group. allow_postgres_traffic.id]
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.public[0].id, aws_subnet.public[1].id]
+
+  tags = {
+    Name = "barnardine subnet group"
+  }
+}
+
+resource "aws_security_group" "allow_postgres_traffic" {
+  name        = "allow_postgres"
+  description = "Allow Postgres traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "allow Postgres"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow postgres"
+  }
+}
+ 
+ resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
+
+variable "subnets_cidr" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["eu-west-2", "eu-west-2"]
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.subnets_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.subnets_cidr, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "routing_table_assoc" {
+  count          = length(var.subnets_cidr)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public-rt.id
 }
